0e2a6b58a89fbb7959c8b6c2ac21372bcdfe81ac
Šimon Procházka <simon.prochazka@me.com>
2016-11-23 14:35:04 +0100

    Bootstrap, jquery and tether loading using loadJS

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,21 @@
+import loadJS from 'fg-loadjs';
 import 'babel-polyfill';
+import 'jquery';
 /**
  * https://github.com/Keyamoon/svgxuse
  * If you do not use SVG <use xlink:href="…"> elements, remove svgxuse module
  */
 import 'svgxuse';
+
 import init from './init';
 import factory from './factory';
 import MyModule from './components/module';
 import Alertifier from './components/alertifier';
+
+loadJS('https://code.jquery.com/jquery-3.1.1.min.js', () => {
+    // eslint-disable-next-line
+    require('./bootstrap-loader');
+});
 
 const app = () => {
     init(MyModule, document.querySelector('.main h1'));
